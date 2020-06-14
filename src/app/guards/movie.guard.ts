98dec8747fa95bd972c2e238ae3f5a1dffca2217
Enea Jahollari <jahollarienea14@gmail.com>
2020-06-14 12:07:38 +0200

    Added query in store

--- src/app/guards/movie.guard.ts
+++ src/app/guards/movie.guard.ts
@@ -3,7 +3,8 @@ import { CanActivate, ActivatedRouteSnapshot } from '@angular/router';
 import { take } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { AppService } from '../services';
-import { Store } from '../store';
+import { State, Store } from '../store';
+import { Movie } from '../models';
 
 @Injectable({ providedIn: 'root' })
 export class MovieGuard implements CanActivate {
@@ -11,6 +12,11 @@ export class MovieGuard implements CanActivate {
   constructor(private store: Store, private appService: AppService) {}
 
   canActivate(next: ActivatedRouteSnapshot): Observable<boolean> | boolean {
+
+    this.store.query<Movie[]>((state) => state.movies).subscribe(data => {
+      console.log(data);
+    });
+
     const movies = this.store.get('movies');
     if (!movies || movies.length === 0 || movies.length === 1) {
       this.appService.getMovies().pipe(take(1)).subscribe();
